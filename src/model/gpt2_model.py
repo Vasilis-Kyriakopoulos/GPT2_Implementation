252e4ab823002f480585e0995cea39dc6de6fcaa
vasil <vasilis133013@gmail.com>
2025-11-17 22:20:32 +0200

    added model classes

---
+++ src/model/gpt2_model.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+from .gpt2_blocks import  GPTBlock_Torch
+
+class GPTModel_Torch(nn.Module):
+    def __init__(self, vocab_size=50257, max_len=1024, embed_dim=768, num_heads=12, ff_dim=3072, num_layers=12, dropout=0.1):
+        super().__init__()
+        self.token_emb = nn.Embedding(vocab_size, embed_dim)
+        self.pos_emb = nn.Embedding(max_len, embed_dim)
+        self.drop = nn.Dropout(dropout)
+        
+        self.blocks = nn.ModuleList([
+            GPTBlock_Torch(embed_dim, num_heads, ff_dim, dropout) for _ in range(num_layers)
+        ])
+        
+        self.ln_f = nn.LayerNorm(embed_dim,)
+        self.head = nn.Linear(embed_dim, vocab_size, bias=False)
+
+    def forward(self, idx, mask=None):
+        B, T = idx.shape
+
+        pos = torch.arange(T, dtype=torch.long, device=idx.device).unsqueeze(0)  # (1, T)
+        # Token + positional embeddings
+        x = self.token_emb(idx) + self.pos_emb(pos)
+        x = self.drop(x)
+
+        # Pass through transformer blocks
+        for block in self.blocks:
+           # x = block(x, attn_mask=mask)
+            x = block(x,mask)
+        # Final layer norm + output head
+        x = self.ln_f(x)
+        logits = self.head(x)
+        return logits
+    
+    def generate(self, idx, max_new_tokens=100):
+        for _ in range(max_new_tokens):
+            logits = self(idx)
+            next_token = torch.argmax(logits[:, -1, :], dim=-1, keepdim=True)
+            idx = torch.cat([idx, next_token], dim=1)
+        return idx
+    
+    
+    
